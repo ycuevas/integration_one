7569f2131e29168a3d0243986ce5c576c2610725
Yaritza Cuevas <yaricuevas2007@gmail.com>
2023-02-28 20:33:53 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Require selenium webdriver
+let webdriver = require("selenium-webdriver");
+
+// Require webdriver for chrome
+// browser called chromedriver
+require("chromedriver");
+
+// Build new window of chrome
+let driver = new webdriver.Builder()
+	.forBrowser("chrome").build();
+
+// Open geeksforgeeks using get method
+driver.get("https://www.geeksforgeeks.org/");
+
